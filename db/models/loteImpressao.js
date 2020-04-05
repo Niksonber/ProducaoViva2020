4a790b3403f2258d242d76b50487af80ea54f5bc
Luiz <labm1997@gmail.com>
2020-04-05 13:34:40 -0300

    Primeira versao com todas as tabelas mapeadas em modelos

--- db/models/loteImpressao.js
+++ db/models/loteImpressao.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   // Declaração da tabela
   var LoteImpressao = sequelize.define('LoteImpressao', {
     data: DataTypes.DATE,
-    qnt_aprovadas: DataTypes.INTEGER,
-    qnt_reprovadas: DataTypes.INTEGER,
+    qtd_aprovadas: DataTypes.INTEGER,
+    qtd_reprovadas: DataTypes.INTEGER,
     cod_impressora: DataTypes.STRING
   }, {timestamps: false, freezeTableName: true});
 
